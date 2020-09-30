a6505b6164fecc3459aeab77804437586091c7b1
Amit Kumar Gupta <58649897+amit873@users.noreply.github.com>
2020-09-30 21:27:14 +0530

    Added Color
    Added Color

--- User-Data-1/index.php
+++ User-Data-1/index.php
@@ -1,26 +1,26 @@
 <html>
 <title> Amit Kumar Gupta DEMO </title>
 <body>
+  <center><p style="font-size:60px"><font color="#ff0000">This is a Demo Web Site</font></p><center>
 <h1>
-This is a Demo Web Site
-<br>
-<IMG SRC="https://raw.githubusercontent.com/amit873/AWS-User-Data/master/Amit-Kumar-Gupta-Logo.png" ALT="Demo Logo"><br>
-<center>
-<?php
-$eip = file_get_contents('http://169.254.169.254/latest/meta-data/public-ipv4');
-$privip = file_get_contents('http://169.254.169.254/latest/meta-data/local-ipv4');
-$lhst = file_get_contents('http://169.254.169.254/latest/meta-data/local-hostname');
-$hst = file_get_contents('http://169.254.169.254/latest/meta-data/public-hostname');
-echo "Public IP: $eip\n";
-echo "<br>";
-echo "Private IP: $privip\n";
-echo "<br>";
-echo "Local Hostname: $lhst\n";
-echo "<br>";
-echo "Public Hostname: $hst\n";
-echo "<br>";
-?>
-</center>
+  <center>
+    <br>
+    <IMG SRC="https://raw.githubusercontent.com/amit873/AWS-User-Data/master/Amit-Kumar-Gupta-Logo.png" ALT="Demo Logo"><br>
+      <?php
+      $eip = file_get_contents('http://169.254.169.254/latest/meta-data/public-ipv4');
+      $privip = file_get_contents('http://169.254.169.254/latest/meta-data/local-ipv4');
+      $lhst = file_get_contents('http://169.254.169.254/latest/meta-data/local-hostname');
+      $hst = file_get_contents('http://169.254.169.254/latest/meta-data/public-hostname');
+      echo "Public IP: $eip\n";
+      echo "<br>";
+      echo "Private IP: $privip\n";
+      echo "<br>";
+      echo "Local Hostname: $lhst\n";
+      echo "<br>";
+      echo "Public Hostname: $hst\n";
+      echo "<br>";
+      ?>
+  </center>
 </h1>
 </body>
 </html>
